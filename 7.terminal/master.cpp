0930652cb58e78bfad081098283afffb24d8a24c
WONGZEONJYU <958323726@qq.com>
2023-06-17 18:53:21 +0800

    add

--- 7.terminal/master.cpp
+++ 7.terminal/master.cpp
@@ -9,29 +9,29 @@
 
 int main(int argc, char *argv[])
 {
-    int master {posix_openpt(O_RDWR)}; // gnome-terminal
+    int master {posix_openpt(O_RDWR)}; /*gnome-terminal*/
     
     if (master > 0){
 
-        grantpt(master);
+        grantpt(master);    /*获取设备使用权限*/
 
-        unlockpt(master);
+        unlockpt(master);   /*解锁设备，为读写做准备*/
 
-        std::cout << "Slave : " << ptsname(master) << "\n";
+        std::cout << "Slave : " << ptsname(master) << "\n"; /*获取伪终端从设备名字*/
 
         int i {},c {};
 
         char rx{},rxbuf[128]{},txbuf[sizeof(rxbuf) * 2]{};
 
-        while (1 == (c = read(master,&rx,1))){
+        while (1 == (c = read(master,&rx,1))){  /*每次读取一个字节*/
 
-            if ('\r' == rx){
+            if ('\r' == rx){    /*遇到回车代表命令一行的结束*/
 
                 rxbuf[i] = 0;
 
-                sprintf(txbuf,"from slave: %s\r",rxbuf);    //show on screen
+                sprintf(txbuf,"from slave: %s\r",rxbuf); /*show on screen*/
 
-                write(master,txbuf,strlen(txbuf));      //keyboaed input
+                write(master,txbuf,strlen(txbuf)); /*keyboaed input*/
 
                 i = 0;
  
--- 7.terminal/slave.cpp
+++ 7.terminal/slave.cpp
@@ -6,25 +6,31 @@
 #include <fcntl.h>
 #include <iostream>
 
-static constexpr char SENDDATA[] {"WONGZEONJYU\r"};
-
 int main(int argc, char const *argv[])
 {
-    int slave {open(argv[1],O_RDWR)};
+    int slave {open(argv[1],O_RDWR)};   //shell
 
     if (slave > -1)
     {
-        char buf[128]{};
+        constexpr char SENDDATA[] {"WONGZEONJYU\r"};
+
         write(slave,SENDDATA,strlen(SENDDATA));
+
         sleep(1);
+
+        char buf[128]{};
+
         int len (read(slave,buf,sizeof(buf) - 1));
+
         buf[(len > 0)? len : 0] = 0;
+
         std::cout << "Read: "<< buf <<"\n";
+
         close(slave);
+
     }else{
         std::cout << "open slave error ...\n";
     }
 
     return 0;
 }
-

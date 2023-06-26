7f5cb06fa97613cd3d93e7451b361859c428bb39
WONGZEONJYU <958323726@qq.com>
2023-06-27 00:24:34 +0800

    add & modify

--- 2.Process/test.cpp
+++ 2.Process/test.cpp
@@ -1,19 +1,31 @@
 #include <cstdio>
 #include <sys/types.h>
 #include <unistd.h>
-#include <iostream>
 
-#define EXE "helloworld.out"
+constexpr char EXE[] {"helloworld.out"};
+
+int create_process(const char* path,char* args[])
+{
+    int ret {fork()};
+
+    if (0 == ret){
+        execve(path,args,nullptr);
+    }
+
+    return ret;
+}
 
 int main(int argc, char const *argv[])
 {
-    char* args[] = {const_cast<char*>(EXE),NULL};
+    char* args[] {const_cast<char*>(EXE),nullptr};
 
-    std::cout << "begin\n";
+    printf("begin\n");
 
-    std::cout << "pid = " << getpid() << "\n";
+    printf("pid = %d\n",getpid());
 
-    execve(EXE,args,NULL);
+    // execve(EXE,args,nullptr);
+
+    printf("child pid = %d\n",create_process(EXE,args));
 
     printf("end\n");
 
---
+++ x-1_Process_Parameter/long.cpp
@@ -0,0 +1,51 @@
+#include <cstdio>
+#include <getopt.h>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int option_index{},flag{-1};
+
+    const option long_options[] {
+        { "add",     required_argument, &flag, 'a' },
+        { "delete",  required_argument, &flag, 'd' }, 
+        { "clear",   no_argument,       &flag, 'c' },
+        { "list",    optional_argument, &flag, 'l' },
+        { 0,         0,                 0,      0  },
+    };
+
+    while( true ){
+
+        int c { getopt_long(argc, argv, "n:ml::", long_options, &option_index) };
+        
+        if( -1 == c ) {
+            break;
+        }
+
+        std::cout << "flag = " << flag << "(" << static_cast<char>(flag) << ")" << 
+            ", c = " << c << "(" << static_cast<char>(c) << ")" << "\n";
+ 
+        if( c ){
+            if( '?' != c ){
+
+                std::cout << "short: option: " << static_cast<char>(c) << ", value : " << (optarg ? optarg : "(null)") << "\n";
+            }
+        }else{
+
+            std::cout << "long: option: " << ( long_options[option_index].name ? long_options[option_index].name : "(null)" ) << 
+                ", value: " << (optarg ? optarg : "(null)") << "\n";
+        }
+    }
+
+   if( optind < argc ){
+
+        std::cout << "parameters:\n";
+
+        while (optind < argc){
+            //printf("%s\n", argv[optind++]);
+            std::cout << argv[optind++] << "\n";
+        }
+    }
+ 
+   return 0;
+}

7d5a441bb9aa5c8e26286e013a77ce4033fd79c4
WONGZEONJYU <958323726@qq.com>
2023-06-26 11:46:34 +0800

    add & modify

--- 9.daemon_process/first-d.cpp
+++ 9.daemon_process/first-d.cpp
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <cstdio>
 #include <cstdlib>
 #include <fcntl.h>
@@ -7,49 +9,58 @@
 
 int main(int argc, char* argv[])
 {
-
     int pid{};
 
-    if( (pid = fork()) > 0 ){
+    if( (pid = fork()) > 0 ){   /*1*/
         std::cout << "parrent pid = " << getpid() << ", ppid = " << getppid() << 
                 ", pgid = " << getpgrp() << ", sid = " << getsid(getpid()) << "\n";
-        std::cout << "new: " << pid << "\n";
+        std::cout << "new pid: " << pid << "\n";
         exit(0);
     }else if( 0 == pid ){
 
-        setsid();
+        setsid();   /*2*/
 
-        if ((pid = fork()) > 0)
-        {
-            
+        if ((pid = fork()) > 0){    /*3*/
+
+            std::cout << "child pid= " << getpid() << ", ppid = " << getppid() << 
+                ", pgid = " << getpgrp() << ", sid = " << getsid(getpid()) << "\n";
+
+            std::cout << "grandson pid = " << pid << "\n";
+
+            exit(0);
+
         }else if (0 == pid){
-
-            unmask(0);
+            /*4*/
+            umask(0);
             chdir("/");
-
+            /*5*/
             close(STDIN_FILENO);
             close(STDOUT_FILENO);
             close(STDERR_FILENO);
-
+            /*6*/
             int i {open("/dev/null",O_RDONLY)}; /* 0 ===> stdin */
-            i+= open("/dev/null",O_WRONLY);     /*1 ===> stdout */
+            i+= open("/home/wong/Linux_System_Program/9.daemon_process/d.log",O_WRONLY);     /*1 ===> stdout */
+            /*不想要任何输出，就把标准输出重定向到 /dev/null */
             i+= open("/dev/null",O_RDWR);       /*2 ===> stderr */
+
+            std::cout << "grandson pid= " << getpid() << ", ppid = " << getppid() << 
+                ", pgid = " << getpgrp() << ", sid = " << getsid(getpid()) << "\n";
+
+            while (true){
+                std::cout << "i = " << i++ << "\n";
+                sleep(1);
+                //fflush(stdout);/*强制把缓冲区数据写入磁盘*/
+            }
 
         }else{
             std::cout << "grandson fork error...\n";
         }
 
-        std::cout << "child = " << getpid() << ", ppid = " << getppid() << 
-                ", pgid = " << getpgrp() << ", sid = " << getsid(getpid()) << "\n";
-        
     }else{
 
         std::cout << "fork error...\n";
     }
 
-    sleep(240);
-
-    
     return 0;
 }
 

7d35f8ec9cd90f00f5472df9896dd725c671a1a0
WONGZEONJYU <958323726@qq.com>
2023-06-28 23:57:47 +0800

    add

---
+++ 10.Signal/signal_reentrant/sysv_signal_demo.cpp
@@ -0,0 +1,40 @@
+//#define _GNU_SOURCE
+// #define _XOPEN_SOURCE   600
+// #define _POSIX_C_SOURCE 200800L
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+void delay_handler(int sig)
+{
+    sysv_signal(SIGINT, delay_handler);
+
+    std::cout << "begin " << __FUNCTION__ << "...\n";
+
+    for (int i {}; i < 5; i++){
+        std::cout << "sleep " << i <<"...\n";
+        sleep(1);
+    }
+
+    std::cout << "end " << __FUNCTION__ << "...\n";
+}
+
+int main(int argc,char* argv[])
+{
+    // signal(SIGINT, signal_handler);
+    // signal(40, signal_handler);
+    sysv_signal(SIGINT, delay_handler);
+    //bsd_signal(SIGINT, signal_handler);/*在cpp文件编译不通过,暂时不知道原因,编译器提示换成ssignal(...)*/
+    //ssignal(SIGINT, signal_handler); 
+
+    while( true ){
+        sleep(1);
+    }
+
+    return 0;
+}
+

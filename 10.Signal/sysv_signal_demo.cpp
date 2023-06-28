71a3b920295714017a6d0144e44d83111d477764
WONGZEONJYU <958323726@qq.com>
2023-06-28 23:06:39 +0800

    add & modify

--- 10.Signal/sysv_signal_demo.cpp
+++ 10.Signal/sysv_signal_demo.cpp
@@ -16,10 +16,9 @@ void signal_handler(int sig)
 
 int main(int argc,char* argv[])
 {
-    signal(SIGINT, signal_handler);
+    // signal(SIGINT, signal_handler);
     // signal(40, signal_handler);
-
-    //sysv_signal(SIGINT, signal_handler);
+    sysv_signal(SIGINT, signal_handler);
     //bsd_signal(SIGINT, signal_handler);/*在cpp文件编译不通过,暂时不知道原因,编译器提示换成ssignal(...)*/
     //ssignal(SIGINT, signal_handler); 
 

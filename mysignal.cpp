fbb0627f466d997772209b5dfbb8a8617c1e1595
WONGZEONJYU <958323726@qq.com>
2023-11-15 20:47:40 +0800

    m

--- mysignal.cpp
+++ mysignal.cpp
@@ -5,9 +5,9 @@
 
 namespace wtd {
 
-    std::map<int,MySignal::_sp_MySignal_type> MySignal::sm_map_;
+    std::unordered_map<int,MySignal::_sp_MySignal_type> MySignal::sm_map_;
 
-    void MySignal::reg_signal(const int sig,const int flags) {
+    MySignal::MySignal(const int sig,const int flags):m_sig_(sig){
 
         m_act_.sa_sigaction = signal_handler;
         m_act_.sa_flags = SA_SIGINFO | flags;
@@ -26,7 +26,6 @@ namespace wtd {
 
     MySignal::~MySignal(){
         if (m_sig_){
-            std::cout << m_hander_.get() << "\n";
             sm_map_.erase(m_sig_);
         }
     }
@@ -42,7 +41,7 @@ namespace wtd {
         return *this;
     }
 
-    int MySignal::sig(int index){
+    int MySignal::sig(const int index){
 
         if (sm_map_.find(index) != sm_map_.end()){
             return sm_map_[index]->m_sig_;
@@ -50,11 +49,11 @@ namespace wtd {
         return -1;
     }
 
-    int MySignal::sig(){
+    int MySignal::sig() const{
         return m_sig_;
     }
 
-    siginfo_t MySignal::siginfo(int index){
+    siginfo_t MySignal::siginfo(const int index){
 
         if (sm_map_.find(index) != sm_map_.end()){
             return sm_map_[index]->m_info_;
@@ -62,27 +61,28 @@ namespace wtd {
         return {};
     }
 
-    siginfo_t MySignal::siginfo(){
+    siginfo_t MySignal::siginfo() const{
         return m_info_;
     }
 
     void MySignal::signal_handler(const int sig,siginfo_t* info,void*) {
 
-        if (sm_map_.find(sig) != sm_map_.end()){
+        if (sm_map_.find(sig) != sm_map_.end()) {
 
             auto _t {sm_map_[sig]};
+
             if (_t) {
                 _t->m_sig_ = sig;
                 _t->m_info_ = *info;
+
                 auto __t {_t->m_hander_};
+
                 if (__t){
                     __t->func();
                 }
             }
         }
     }
-
-    void MySignal::signal_proxy(void* ){
-        
-    }
 }
+
+
--- mysignal.hpp
+++ mysignal.hpp
@@ -2,8 +2,8 @@
 #define MY_SIGNAL_H
 
 #include <tuple>
-//#include <unordered_map>
-#include <map>
+#include <unordered_map>
+//#include <map>
 #include <memory>
 #include <type_traits>
 #include <signal.h>
@@ -39,60 +39,17 @@ class MySignal
         return _sp_base_type{new _Impl{std::forward<_Callable>(__f)}};
     }
 
-    static void signal_handler(int,siginfo_t* ,void*);
-    static void signal_proxy(void*);
-    void reg_signal(const int sig,const int flags);
-
-    template<typename Fn,typename... Args>
-    void init(Fn&& fn,Args&& ...args){
-        auto t {__make_invoker(std::forward<Fn>(fn),std::forward<Args>(args)...)};
-        auto _sp_t {_S_make_state(t)};
-        m_hander_ = std::move(_sp_t);
-    }
-
-    template<typename Fn,typename... Args>
-    explicit MySignal(const int sig,const int flags,Fn&& fn,Args&& ...args):m_sig_(sig){
-        reg_signal(sig,flags);
-        init(std::forward<Fn>(fn),std::forward<Args>(args)...);
-    }
-
-    //MySignal() = default;
-
-public:
-    using _sp_MySignal_type = std::shared_ptr<MySignal>;
-
-    template<typename Fn,typename... Args>
-    static _sp_MySignal_type Create(const int sig,const int flags,Fn&& fn,Args&& ...args){
-        
-        auto r {_sp_MySignal_type(new MySignal(sig,flags,std::forward<Fn>(fn),std::forward<Args>(args)...))};
-        sm_map_.insert({sig,r});
-        return r;
-    }
-
-    ~MySignal();
-
-    MySignal(MySignal&&);
-    MySignal& operator=(MySignal&&);
-
-    static int sig(int);
-    int sig();
-    static siginfo_t siginfo(int);
-    siginfo_t siginfo();
-
-private:
-
     template<typename _Tuple>
     struct _Invoker {
 	    _Tuple _M_t;
 
-        template<typename>
-        struct __result;
-
+        template<typename> struct __result;
+        
         template<typename _Fn, typename... _Args>
         struct __result<std::tuple<_Fn, _Args...>> : std::__invoke_result<_Fn, _Args...>{ };
 
         template<size_t... _Ind>
-        typename __result<_Tuple>::type _M_invoke(std::_Index_tuple<_Ind...>) { 
+        typename __result<_Tuple>::type _M_invoke(std::_Index_tuple<_Ind...>) {
             return std::__invoke(std::get<_Ind>(std::move(_M_t))...); 
         }
 
@@ -100,6 +57,7 @@ private:
             using _Indices = typename std::_Build_index_tuple<std::tuple_size<_Tuple>::value>::__type;
             return _M_invoke(_Indices());
         }
+
     };
 
     template<typename... _Tp>
@@ -110,14 +68,57 @@ private:
         return { __decayed_tuple<_Callable, _Args...>{std::forward<_Callable>(__callable), std::forward<_Args>(__args)...} };
     }
 
+    static void signal_handler(int,siginfo_t* ,void*);
+
+    template<typename Fn,typename... Args>
+    void init(Fn&& fn,Args&& ...args){
+        auto _invoker {__make_invoker(std::forward<Fn>(fn),std::forward<Args>(args)...)};
+        auto _sp_t {_S_make_state(std::forward<decltype(_invoker)>(_invoker))};
+        m_hander_ = std::move(_sp_t);
+    }
+
+    template<typename Fn,typename... Args>
+    explicit MySignal(const int sig,const int flags,Fn&& fn,Args&& ...args):
+            MySignal(sig,flags){
+        init(std::forward<Fn>(fn),std::forward<Args>(args)...);
+    }
+
+    MySignal(const int sig,const int flags);
+
+    MySignal() = delete;
+
+public:
+    
+    using _sp_MySignal_type = std::shared_ptr<MySignal>;
+
+    template<typename Fn,typename... Args>
+    [[nodiscard]] static const _sp_MySignal_type Create(const int sig,const int flags,
+                                                Fn&& fn,Args&& ...args){
+        
+        const auto r {_sp_MySignal_type(new MySignal(sig,flags,std::forward<Fn>(fn),std::forward<Args>(args)...))};
+        sm_map_.insert({sig,r});
+        return r;
+    }
+
+    ~MySignal();
+
+    MySignal(MySignal&&);
+    MySignal& operator=(MySignal&&);
+
+    static int sig(int) ;
+    int sig()const ;
+    static siginfo_t siginfo(int);
+    siginfo_t siginfo() const;
+
 private:
     int m_sig_{};
     struct sigaction m_act_{};
     siginfo_t m_info_{};
     _sp_base_type m_hander_{};
-    static std::map<int,_sp_MySignal_type> sm_map_;
+    static std::unordered_map<int,_sp_MySignal_type> sm_map_;
 };
 
 }
+
 
 #endif

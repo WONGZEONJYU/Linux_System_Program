18ec376b824fc566a8c2f71652dd140187f17845
WONGZEONJYU <958323726@qq.com>
2023-11-25 23:39:06 +0800

    m

--- mysignal.cpp
+++ mysignal.cpp
@@ -10,31 +10,10 @@ namespace wtd {
         sigaction (sig, &m_act_,nullptr);
     }
 
-    void MySignal::SWap(MySignal&& obj){
-        m_sig_ = obj.m_sig_;
-        m_act_ = obj.m_act_;
-        m_info_ = obj.m_info_;
-        m_hander_ = std::move(obj.m_hander_);
-        obj.m_sig_ = 0;
-        memset(&obj.m_act_,0,sizeof(obj.m_act_));
-        memset(&obj.m_info_,0,sizeof(obj.m_info_));
-    }
-
     MySignal::~MySignal(){
         if (m_sig_){
             sm_map_.erase(m_sig_);
         }
-    }
-
-    MySignal::MySignal(MySignal&& obj){
-        SWap(std::move(obj));
-    }
-
-    MySignal& MySignal::operator=(MySignal&& obj) {
-        if (std::addressof(obj) != this) {
-            SWap(std::move(obj));
-        }
-        return *this;
     }
 
     int MySignal::sig(const int index){

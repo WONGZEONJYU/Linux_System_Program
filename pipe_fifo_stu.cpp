affdf0f1e57759e427936fbcfef4f2b784539459
WONGZEONJYU <958323726@qq.com>
2023-11-27 18:12:08 +0800

    add

---
+++ pipe_fifo_stu.cpp
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+using namespace std;
+static int test1();
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return test1();
+}
+
+static int test1()
+{
+
+    
+
+
+
+
+    return 0;
+}

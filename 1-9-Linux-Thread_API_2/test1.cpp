38c54bbe83dc6ecc1362cd3e9abd80c17d8bad24
WONGZEONJYU <958323726@qq.com>
2024-01-09 17:23:47 +0800

    add

---
+++ 1-9-Linux-Thread_API_2/test1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+#include <cstring>
+
+using namespace std;
+using namespace chrono;
+using namespace this_thread;
+
+void clean_test(void* arg)
+{
+    cout << __FUNCTION__ << " : " << reinterpret_cast<long long>(arg) << "\n";
+}
+
+void foo()
+{
+    cout << __FUNCTION__ << " begin\n";
+    cout << "I'm " << __FUNCTION__ << "\n" << flush;
+    //pthread_exit(nullptr);
+    cout << __FUNCTION__ << " end\n";
+}
+
+void* thread_entry(void* arg)
+{
+    pthread_cleanup_push(clean_test,reinterpret_cast<void*>(111));
+
+    foo();
+
+    pthread_cleanup_pop(1);
+
+    return nullptr;
+}
+
+int main(int argc, char const *argv[])
+{
+    pthread_t t{};
+    pthread_create(&t,nullptr,thread_entry,nullptr);
+    pthread_join(t,nullptr);
+    return 0;
+}
